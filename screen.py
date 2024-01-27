4b40070d9a81261654910b526e82511e620d0346
GigaMami126 <157909445+GigaMami126@users.noreply.github.com>
2024-01-27 20:54:12 +0300

    Create screen.py

---
+++ screen.py
@@ -0,0 +1,10 @@
+from tkinter import*
+
+def create_window():
+    new_window = Toplevel()
+
+window = Tk()
+
+Button(window, text="create new window", command=create_window).pack()
+
+window.mainlo
